fe2b9635aaeb74b05779900e12c71e00ef4d11b1
Gusionling <isdnhk@naver.com>
2024-10-29 20:14:34 +0900

    initial commit

---
+++ src/main/java/YakDaBang/Yakdabang/YakdabangApplication.java
@@ -0,0 +1,13 @@
+package YakDaBang.Yakdabang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YakdabangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YakdabangApplication.class, args);
+	}
+
+}
